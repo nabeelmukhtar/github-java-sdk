06294f6904ef087db26bbacf29b6591603344f88
Nabeel Mukhtar <nabeelmukhtar@yahoo.com>
2010-07-14 18:06:05 +0500

    Removed the dependency on commons-cli.

--- examples/src/java/com/github/api/v2/services/example/GistApiSample.java
+++ examples/src/java/com/github/api/v2/services/example/GistApiSample.java
@@ -7,15 +7,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.text.MessageFormat;
-
-import org.apache.commons.cli.BasicParser;
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.HelpFormatter;
-import org.apache.commons.cli.Option;
-import org.apache.commons.cli.OptionBuilder;
-import org.apache.commons.cli.Options;
-import org.apache.commons.cli.ParseException;
 
 import com.github.api.v2.schema.Gist;
 import com.github.api.v2.services.GistService;
@@ -26,98 +17,23 @@ import com.github.api.v2.services.GitHubServiceFactory;
  */
 public class GistApiSample {
 
-    /** The Constant APPLICATION_KEY_OPTION. */
-    private static final String APPLICATION_KEY_OPTION = "appid";
-	
-    /** The Constant QUERY_OPTION. */
-    private static final String QUERY_OPTION = "query";
-	
-    /** The Constant HELP_OPTION. */
-    private static final String HELP_OPTION = "help";
-    
     /**
      * The main method.
      * 
      * @param args the arguments
      */
 	public static void main(String[] args) {
-		Options options = buildOptions();
-        try {
-            CommandLine line = new BasicParser().parse(options, args);
-            processCommandLine(line, options);
-        } catch(ParseException exp ) {
-            System.err.println(exp.getMessage());
-            printHelp(options);
-        }
+		GitHubServiceFactory factory = GitHubServiceFactory.newInstance();
+		GistService service = factory.createGistService();
+		Gist gist = service.getGist("289179");
+		printResult(gist);
+		System.out.println(convertStreamToString(service.getGistContent("289179", "TimeZoneDSTUtil.java")));
 	}
     
-	/**
-	 * Process command line.
-	 * 
-	 * @param line the line
-	 * @param options the options
-	 */
-    private static void processCommandLine(CommandLine line, Options options) {
-        if(line.hasOption(HELP_OPTION)) {
-            printHelp(options);            
-        } // else if(line.hasOption(APPLICATION_KEY_OPTION) && line.hasOption(QUERY_OPTION)) 
-        {
-    		GitHubServiceFactory factory = GitHubServiceFactory.newInstance();
-    		GistService service = factory.createGistService();
-    		Gist gist = service.getGist("289179");
-    		printResult(gist);
-    		System.out.println(convertStreamToString(service.getGistContent("289179", "TimeZoneDSTUtil.java")));
-//        } else {
-//        	printHelp(options);
-        }
-	}
-
 	private static void printResult(Gist gist) {
 		System.out.println(gist);
 	}
 
-	/**
-	 * Builds the options.
-	 * 
-	 * @return the options
-	 */
-    private static Options buildOptions() {
-       
-        Options opts = new Options();
-        
-        String helpMsg = "Print this message.";
-        Option help = new Option(HELP_OPTION, helpMsg);
-        opts.addOption(help);
-
-        String applicationKeyMsg = "You Application ID.";
-        OptionBuilder.withArgName("appid");
-        OptionBuilder.hasArg();
-        OptionBuilder.withDescription(applicationKeyMsg);
-        Option applicationKey = OptionBuilder.create(APPLICATION_KEY_OPTION);
-        opts.addOption(applicationKey);
-        
-        String queryMsg = "Search Query.";
-        OptionBuilder.withArgName("query");
-        OptionBuilder.hasArg();
-        OptionBuilder.withDescription(queryMsg);
-        Option query = OptionBuilder.create(QUERY_OPTION);
-        opts.addOption(query);
-        
-        return opts;
-    }
-    
-    /**
-     * Prints the help.
-     * 
-     * @param options the options
-     */
-    private static void printHelp(Options options) {
-        int width = 80;
-        String syntax = GistApiSample.class.getName() + " <options>";
-        String header = MessageFormat.format("\nThe -{0} and -{1} options are required. All others are optional.", APPLICATION_KEY_OPTION, QUERY_OPTION);
-        new HelpFormatter().printHelp(width, syntax, header, options, null, false);
-    }
-    
 	/**
 	 * Convert stream to string.
 	 * 
--- examples/src/java/com/github/api/v2/services/example/ObjectApiSample.java
+++ examples/src/java/com/github/api/v2/services/example/ObjectApiSample.java
@@ -7,16 +7,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.text.MessageFormat;
 import java.util.List;
-
-import org.apache.commons.cli.BasicParser;
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.HelpFormatter;
-import org.apache.commons.cli.Option;
-import org.apache.commons.cli.OptionBuilder;
-import org.apache.commons.cli.Options;
-import org.apache.commons.cli.ParseException;
 
 import com.github.api.v2.schema.Blob;
 import com.github.api.v2.schema.Tree;
@@ -28,58 +19,25 @@ import com.github.api.v2.services.ObjectService;
  */
 public class ObjectApiSample {
 
-    /** The Constant APPLICATION_KEY_OPTION. */
-    private static final String APPLICATION_KEY_OPTION = "appid";
-	
-    /** The Constant QUERY_OPTION. */
-    private static final String QUERY_OPTION = "query";
-	
-    /** The Constant HELP_OPTION. */
-    private static final String HELP_OPTION = "help";
-    
     /**
      * The main method.
      * 
      * @param args the arguments
      */
 	public static void main(String[] args) {
-		Options options = buildOptions();
-        try {
-            CommandLine line = new BasicParser().parse(options, args);
-            processCommandLine(line, options);
-        } catch(ParseException exp ) {
-            System.err.println(exp.getMessage());
-            printHelp(options);
-        }
+		GitHubServiceFactory factory = GitHubServiceFactory.newInstance();
+		ObjectService service = factory.createObjectService();
+		List<Tree> trees = service.getTree("facebook", "tornado", "7b80c2f4db226d6fa3a7");
+		for (Tree tree : trees) {
+			printResult(tree);    			
+		}
+		List<Blob> blobs = service.getBlobs("facebook", "tornado", "7b80c2f4db226d6fa3a7");
+		for (Blob blob : blobs) {
+			printResult(blob);
+		}
+		System.out.println(convertStreamToString(service.getObjectContent("facebook", "tornado", "7b80c2f4db226d6fa3a7f3dfa59277da1d642f91")));
 	}
     
-	/**
-	 * Process command line.
-	 * 
-	 * @param line the line
-	 * @param options the options
-	 */
-    private static void processCommandLine(CommandLine line, Options options) {
-        if(line.hasOption(HELP_OPTION)) {
-            printHelp(options);            
-        } // else if(line.hasOption(APPLICATION_KEY_OPTION) && line.hasOption(QUERY_OPTION)) 
-        {
-    		GitHubServiceFactory factory = GitHubServiceFactory.newInstance();
-    		ObjectService service = factory.createObjectService();
-    		List<Tree> trees = service.getTree("facebook", "tornado", "7b80c2f4db226d6fa3a7");
-    		for (Tree tree : trees) {
-    			printResult(tree);    			
-    		}
-    		List<Blob> blobs = service.getBlobs("facebook", "tornado", "7b80c2f4db226d6fa3a7");
-    		for (Blob blob : blobs) {
-    			printResult(blob);
-    		}
-    		System.out.println(convertStreamToString(service.getObjectContent("facebook", "tornado", "7b80c2f4db226d6fa3a7f3dfa59277da1d642f91")));
-//        } else {
-//        	printHelp(options);
-        }
-	}
-
 	private static void printResult(Blob blob) {
 		System.out.println(blob);
 	}
@@ -88,48 +46,6 @@ public class ObjectApiSample {
 		System.out.println(tree);
 	}
 
-	/**
-	 * Builds the options.
-	 * 
-	 * @return the options
-	 */
-    private static Options buildOptions() {
-       
-        Options opts = new Options();
-        
-        String helpMsg = "Print this message.";
-        Option help = new Option(HELP_OPTION, helpMsg);
-        opts.addOption(help);
-
-        String applicationKeyMsg = "You Application ID.";
-        OptionBuilder.withArgName("appid");
-        OptionBuilder.hasArg();
-        OptionBuilder.withDescription(applicationKeyMsg);
-        Option applicationKey = OptionBuilder.create(APPLICATION_KEY_OPTION);
-        opts.addOption(applicationKey);
-        
-        String queryMsg = "Search Query.";
-        OptionBuilder.withArgName("query");
-        OptionBuilder.hasArg();
-        OptionBuilder.withDescription(queryMsg);
-        Option query = OptionBuilder.create(QUERY_OPTION);
-        opts.addOption(query);
-        
-        return opts;
-    }
-    
-    /**
-     * Prints the help.
-     * 
-     * @param options the options
-     */
-    private static void printHelp(Options options) {
-        int width = 80;
-        String syntax = ObjectApiSample.class.getName() + " <options>";
-        String header = MessageFormat.format("\nThe -{0} and -{1} options are required. All others are optional.", APPLICATION_KEY_OPTION, QUERY_OPTION);
-        new HelpFormatter().printHelp(width, syntax, header, options, null, false);
-    }
-    
 	/**
 	 * Convert stream to string.
 	 * 
